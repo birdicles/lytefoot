716395af403146d389c95c0aa49630946c8b4d33
Nelson Shin <nsreceipts@yahoo.com>
2016-02-19 22:29:05 -0800

    add some map config

--- ios/src/components/views/MapComponentView.js
+++ ios/src/components/views/MapComponentView.js
@@ -1,5 +1,13 @@
 // https://facebook.github.io/react-native/docs/mapview.html#content
-import React, { Component, PropTypes, StyleSheet, MapView, Text, View } from 'react-native';
+import React, {
+    Component,
+    PropTypes,
+    StyleSheet,
+    MapView,
+    Text,
+    View,
+    TouchableOpacity
+} from 'react-native';
 
 export default class MapComponentView extends Component {
 
@@ -8,11 +16,72 @@ export default class MapComponentView extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            coords: {
+                latitude: -100,
+                longitude: -122
+            }
+            // overlays: [{
+            //     coordinates: [
+            //         {
+            //             latitude: 0,
+            //             longitude: 0
+            //         },
+            //         {
+            //             latitude: 100,
+            //             longitude: 100
+            //         }
+            //     ],
+            //     lineWidth: 5,
+            //     strokeColor: '#ff0000',
+            //     // fillColor: [styles.map.borderColor],
+            //     id: 'default'
+            // }]
+        };
     }
 
     componentWillReceiveProps(props) {}
 
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                let initialPosition = position;
+                console.log('MapComponentView.js: initialPosition:', initialPosition);
+                // this.setState(initialPosition);
+            },
+            (error) => alert(error.message),
+            {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 1000
+            }
+        );
+
+        // this.watchID = navigator.geolocation.watchPosition((position) => {
+        //     let lastPosition = JSON.stringify(position);
+        //     this.setState({lastPosition});
+        // });
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchID);
+    }
+
+
+
+    onStopClick() {}
+
+    onPauseClick() {}
+
+    onPlayClick() {}
+
+    // WRAP MAP COMPONENT WITH:
+    // - startOverlay
+    // - updateOverlay
+
     render() {
+        console.log('MapComponentView.js: this.state.coords:', this.state.coords);
         return (
             <View style={styles.container}>
                 <Text style={styles.title}>
@@ -20,8 +89,18 @@ export default class MapComponentView extends Component {
                 </Text>
                 <MapView
                     style={styles.map}
-                    region={this.props.region}
+                    region={this.state.coords}
+                    overlays={this.state.overlays}
                 />
+                <TouchableOpacity onPress={this.onStopClick}>
+                    <Text style={styles.item}>Stop</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.onPauseClick}>
+                    <Text style={styles.item}>Pause</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.onPlayClick}>
+                    <Text style={styles.item}>Play</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -29,6 +108,7 @@ export default class MapComponentView extends Component {
 }
 
 MapComponentView.defaultProps = {};
+
 MapComponentView.propTypes = {
     title: PropTypes.string.isRequired,
     region: PropTypes.object.isRequired
@@ -49,5 +129,11 @@ const styles = StyleSheet.create({
         margin: 20,
         borderWidth: 1,
         borderColor: '#666'
+    },
+    item: {
+        fontSize: 14,
+        fontWeight: '300',
+        fontFamily: 'Avenir-Book',
+        paddingTop: 5
     }
 });
